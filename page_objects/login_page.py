1b6dcb832f592db9be2e064f504ba483cf9e3b9d
slobodanka.mihajlov <mihajlov.slobodanka@yahoo.com>
2024-10-08 12:09:18 +0200

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: slobodanka.mihajlov <mihajlov.slobodanka@yahoo.com>

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -3,28 +3,23 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
+from page_objects.base_page import BasePage
 
-class LoginPage:
+
+class LoginPage(BasePage):
     __url = "https://practicetestautomation.com/practice-test-login/"
     __username_field = (By.ID, "username")
     __password_field = (By.NAME, "password")
     __submit_button = (By.XPATH, "//button[@class='btn']")
 
-
-
-
     def __init__(self,driver: WebDriver):
-        self._driver=driver
+        super().__init__(driver)
 
     #open metod
     def open(self):
-        self._driver.get(self.__url)
+        super().open_url(self.__url)
 
     def execute_login(self, username: str, password: str):
-        wait = WebDriverWait(self._driver, 10)
-        wait.until(ec.visibility_of_element_located(self.__username_field))
-        self._driver.find_element(self.__username_field).send_keys(username)
-        wait.until(ec.visibility_of_element_located(self.__password_field))
-        self._driver.find_element(self.__password_field).send_keys(password)
-        wait.until(ec.visibility_of_element_located(self.__submit_button))
-        self._driver.find_element(self.__submit_button).click()
+        super()._type(self.__username_field, username)
+        super()._type(self.__password_field, password)
+        super()._click(self.__submit_button)
